c8b0acbdc79510bbd54a66b4163d6ae3d9def194
redashu <ashutoshh@linux.com>
2024-05-30 19:46:49 +0530

    day4-cicd

---
+++ ecr/test/ect.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ect from '../lib/ect-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ect-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ect.EctStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
